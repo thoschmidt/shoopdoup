74f5dc57ababf2ec1e5a24df39fdc9df0817f83b
Aaron Konigsberg <akonigsberg@gmail.com>
2012-02-21 23:43:33 -0800

    Draws lines to hand, and began to extract out target classes.

---
+++ P3/ImageSelection.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Drawing;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Timers;
+using Microsoft.Research.Kinect.Nui;
+using Coding4Fun.Kinect.Wpf;
+
+namespace ShoopDoup
+{
+    class ImageManager
+    {
+        public ImageManager()
+        {
+        }
+
+
+    }
+
+    class ImageTarget
+    {
+        private String _filename;
+        private Grid grid;
+        private UIElement image;
+
+        public ImageTarget(String filename)
+        {
+            image = new System.Windows.Shapes.Rectangle();
+            grid = new Grid();
+
+            System.Windows.Shapes.Rectangle rect = ((System.Windows.Shapes.Rectangle)image);
+            rect.StrokeThickness = 5;
+            rect.Stroke = new SolidColorBrush(System.Windows.Media.Color.FromRgb(0, 0, 0));
+
+            grid.Children.Add(rect);
+        }
+        
+        public void setPosition(int x, int y) {
+            Canvas.SetLeft(grid, x);
+            Canvas.SetTop(grid, y);
+        }
+
+        public void setVisibility(bool isVisible)
+        {
+            if (isVisible)
+            {
+                grid.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                grid.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        public void setHighlighted(bool isHighlighted)
+        {
+
+        }
+
+        public Grid getGrid() 
+        {
+            return grid;
+        }
+
+    }
+}
--- P3/ShoopDoupController.cs
+++ P3/ShoopDoupController.cs
@@ -7,17 +7,28 @@ using System.Windows.Media;
 using System.Windows.Shapes;
 using System.Windows.Controls;
 using System.Timers;
+using System.Drawing;
 using Microsoft.Research.Kinect.Nui;
 using Coding4Fun.Kinect.Wpf;
+using ShoopDoup;
 
 namespace SkeletalTracking
 {
     class ShoopDoupController : SkeletonController
     {
-
+        MainWindow win;
         public ShoopDoupController(MainWindow win)
             : base(win)
         {
+            this.win = win;
+            this.line = new System.Windows.Shapes.Line();
+            this.win.MainCanvas.Children.Add(line);
+
+            this.newImageTarget = new ImageTarget("poop");
+            newImageTarget.setPosition(100, 100);
+            this.win.MainCanvas.Children.Add(newImageTarget.getGrid());
+            
+
             rightHandTimer = null;
             rightHandTarget = null;
             rightHandTargetID = -1;
@@ -26,19 +37,21 @@ namespace SkeletalTracking
 
         double highlightedHandBaseDepth;
         double depthDeltaForSelection = .3;
+        Target selectedTarget;
+        ImageTarget newImageTarget;
+        System.Windows.Shapes.Line line;
 
         public override void processSkeletonFrame(SkeletonData skeleton, Dictionary<int, Target> targets)
         {
             Joint chest = skeleton.Joints[JointID.ShoulderCenter].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
             highlightedHandBaseDepth = chest.Position.Z;
 
+            Joint rightHand = skeleton.Joints[JointID.HandRight].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
+
             foreach (var target in targets)
             {
                 Target cur = target.Value;
                 int targetID = cur.id; //ID in range [1..5]
-
-                //Scale the joints to the size of the window
-                Joint rightHand = skeleton.Joints[JointID.HandRight].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
 
                 //Calculate how far our right hand is from the target in both x and y directions
                 double deltaX_right = Math.Abs(rightHand.Position.X - cur.getXPosition());
@@ -53,6 +66,8 @@ namespace SkeletalTracking
                         if (cur.isHighlighted() || cur.isSelected())
                         {
                             cur.setTargetSelected();
+
+                            selectedTarget = cur;
                         }
                         else
                         {
@@ -68,6 +83,25 @@ namespace SkeletalTracking
                 {
                     cur.setTargetUnselected();
                 }
+            }
+
+            if (selectedTarget != null && (Math.Abs(rightHand.Position.Z - highlightedHandBaseDepth) > depthDeltaForSelection))
+            {
+                line.Visibility = System.Windows.Visibility.Visible;
+                line.X1 = selectedTarget.getXPosition();
+                line.X2 = rightHand.Position.X;
+                line.Y1 = selectedTarget.getYPosition();
+                line.Y2 = rightHand.Position.Y;
+                Console.WriteLine("Origin: " + line.X1 + " Hand Location: " + line.X2);
+                line.StrokeThickness = 5;
+                line.Stroke = new SolidColorBrush(System.Windows.Media.Color.FromRgb(0, 0, 0));
+
+
+            }
+            else
+            {
+                line.Visibility = System.Windows.Visibility.Hidden;
+                selectedTarget = null;
             }
 
             /* (firstTimeSeeingSkeleton)
