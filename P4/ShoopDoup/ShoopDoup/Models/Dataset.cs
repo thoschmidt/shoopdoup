4d84a10494dde6302d3dd7dbf4ba8966e3774035
unknown <JohnReidy@JohnReidy-PC.(none)>
2012-02-23 21:02:47 -0800

    Initial commit for basic models.

---
+++ P4/ShoopDoup/ShoopDoup/Models/Dataset.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoopDoup.Models
+{
+    public enum DATASET_TYPES { YES_NO, ASSOCIATE, RATING, INFORMATIVE };
+
+    class Dataset
+    {
+        private int type;
+    }
+}
